a26e8904ba4d075c1c32fb15348f5a6aa3a4d7ab
Sam Ruberti <samruberti@gmail.com>
2015-09-18 17:24:40 -0400

    Add elixir IEx config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 cp ./vim_settings/vimrc.local ~/.vimrc.local
 cp ./vim_settings/vimrc.bundles.local ~/.vimrc.bundles.local
 
+# Elixir
+cp ./elixir_settings/iex.exs ~/.iex.exs
+
 # git aliases
 cp ./zsh_settings/git-aliases ~/.git-aliases
 
